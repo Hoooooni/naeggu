e7a7ab44707f4efbcb92351a57b5659c3e1bac76
kjh38zzang <kjh38zzang@naver.com>
2019-12-05 17:24:09 +0900

    TEST

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
     public static void main(String[] args) {
+        System.out.println("TEST 중임");
         SpringApplication.run(DemoApplication.class, args);
     }
 
